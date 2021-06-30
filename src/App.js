4a2a2b7e803d65f8313522c9264d2038213fb972
sergeisaz <sergeisaz53@gmail.com>
2021-06-30 10:20:09 +0300

    view list movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { moviesData } from './moviesData'
+import MovieItem from './components/MovieItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: moviesData
+    }
+  }
+  removeMovie(movie) {
+    const updateMovies = this.state.movies.filter(function (item) {
+      return item.id !== movie.id;
+    })
+    console.log(updateMovies)
+    this.setState({
+      movies: updateMovies
+    })
+  }
+
+  render() {
+
+    return <div>
+      {this.state.movies.map(movie => {
+        return (
+          <MovieItem
+            key={movie.id}
+            movie={movie}
+            removeMovie={this.removeMovie}
+          />
+
+
+        );
+      })}
     </div>
-  );
+  }
 }
 
 export default App;

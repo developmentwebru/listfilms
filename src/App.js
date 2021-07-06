106f0c4d7d5c8db23ed9cad8b576ba61ecd48c94
sergeisaz <sergeisaz53@gmail.com>
2021-07-06 13:43:18 +0300

    add pagination add classnames extension

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends React.Component {
     this.state = {
       movies: [],
       moviesWillWatch: [],
-      sort_by: "popularity.desc"
+      sort_by: "popularity.desc",
+      total_pages: 500,
+      currentPage: 1
     }
   }
 
@@ -22,23 +24,28 @@ class App extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log("didUpdate")
-    console.log("prev", prevProps, prevState)
-    console.log("this", this.props, this.state)
+    //console.log("didUpdate")
+    // console.log("prev", prevProps, prevState)
+    // console.log("this", this.props, this.state)
     if (prevState.sort_by !== this.state.sort_by) {
+      this.getMovies()
+    }
+    if (prevState.currentPage !== this.state.currentPage) {
       this.getMovies()
     }
   }
 
   getMovies = () => {
-    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`)
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}&page=${this.state.currentPage}`)
       .then((response) => {
         return response.json()
       })
       .then((data) => {
         console.log('data', data)
         this.setState({
-          movies: data.results
+          movies: data.results,
+          total_pages: data.total_pages,
+          currentPage: data.page
         })
       })
   }
@@ -47,7 +54,7 @@ class App extends React.Component {
     const updateMovies = this.state.movies.filter(function (item) {
       return item.id !== movie.id;
     })
-    console.log(updateMovies)
+    //console.log(updateMovies)
     this.setState({
       movies: updateMovies
     })
@@ -55,7 +62,7 @@ class App extends React.Component {
 
   addMovieToWillWatch = movie => {
     // this.state.moviesWillWatch.push(movie)
-    console.log(movie)
+    //console.log(movie)
     // const updateMoviesWillWatch = [...this.state.moviesWillWatch]
     // updateMoviesWillWatch.push(movie);
     const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie]
@@ -71,7 +78,7 @@ class App extends React.Component {
     const updatemoviesWillWatch = this.state.moviesWillWatch.filter(function (item) {
       return item.id !== movie.id;
     })
-    console.log(updatemoviesWillWatch)
+    // console.log(updatemoviesWillWatch)
     this.setState({
       moviesWillWatch: updatemoviesWillWatch
     })
@@ -79,14 +86,42 @@ class App extends React.Component {
 
   updateSortBy = value => {
     this.setState({
-      sort_by: value
+      sort_by: value,
+      currentPage: 1
     });
   };
 
- 
+  handleNext = () => {
+    let changePage = this.state.currentPage + 1
+    if (changePage >= this.state.total_pages) {
+      this.setState({
+        currentPage: this.state.total_pages
+      })
+    } else {
+      this.setState({
+        currentPage: changePage
+      })
+    }
+  }
+
+  handlePrev = () => {
+    let changePage = this.state.currentPage - 1
+    if (changePage <= 1) {
+      this.setState({
+        currentPage: 1
+      })
+    } else {
+      this.setState({
+        currentPage: changePage
+      })
+    }
+
+  }
+
+
 
   render() {
-    console.log("render", this.state.sort_by)
+    //console.log("render", this.state.sort_by)
     return (
       <div className='container'>
         <div className='row mt-4' >
@@ -98,6 +133,22 @@ class App extends React.Component {
                   updateSortBy={this.updateSortBy} />
               </div>
             </div>
+            <nav aria-label="...">
+              <ul className="pagination">
+                <li className="page-item ">
+                  <button className="page-link" onClick={this.handlePrev}> Previous</button>
+                </li>
+                <li className="page-item active" aria-current="page">
+                  <button className="page-link" >{this.state.currentPage}</button></li>
+                <li className="page-item">
+                  <button className="page-link" >{this.state.total_pages}</button>
+                </li>
+
+                <li className="page-item">
+                  <button className="page-link" onClick={this.handleNext}>Next</button>
+                </li>
+              </ul>
+            </nav>
             <div className='row'>
               {this.state.movies.map(movie => {
                 return (
@@ -116,8 +167,10 @@ class App extends React.Component {
           <div className='col-3'>
             <p>Will Watch: {this.state.moviesWillWatch.length}</p>
           </div>
+
+
         </div>
-      </div>
+      </div >
     )
   }
 }
--- src/components/MovieTabs.js
+++ src/components/MovieTabs.js
@@ -1,6 +1,6 @@
 
 import React from "react"
-
+import classNames from "classnames"
 class MovieTabs extends React.Component {
     shouldComponentUpdate(nextProps, nextState) {
         if (nextProps.sort_by !== this.props.sort_by) {
@@ -11,43 +11,43 @@ class MovieTabs extends React.Component {
     }
 
     render() {
-
         const handleClick = (value) => {
             return () => {
                 updateSortBy(value)
             }
         }
 
-        const getClassByValue = value => {
-            return `nav-link ${sort_by === value ? "active" : ""}`
-        }
+        // const getClassByValue = value => {
+        //     return `nav-link ${sort_by === value ? "active" : ""}`
+        // }
 
-        const { sort_by, updateSortBy } = this.props;
+        const { updateSortBy } = this.props;
         return (
             <ul className="tabs nav nav-pills">
                 <li className='nav-item'>
                     <div
                         onClick={handleClick("popularity.desc")}
-                        className={getClassByValue("popularity.desc")}>
+                        className={classNames({ 'nav-link': true, 'active': (this.props.sort_by === 'popularity.desc') })}
+                    >
                         Popularity desc
                     </div>
                 </li>
                 <li className='nav-item'>
                     <div
                         onClick={handleClick("revenue.desc")}
-                        className={getClassByValue("revenue.desc")}>
+                        className={classNames({ 'nav-link': true, 'active': (this.props.sort_by === 'revenue.desc') })}>
                         Revenue desc
                     </div>
                 </li>
                 <li className='nav-item'>
                     <div
                         onClick={handleClick("vote_average.desc")}
-                        className={getClassByValue("vote_average.desc")}>
+                        className={classNames({ 'nav-link': true, 'active': (this.props.sort_by === 'vote_average.desc') })}>
                         Vote avetage desc
                     </div>
                 </li>
 
-            </ul>
+            </ul >
         )
     }
 }
